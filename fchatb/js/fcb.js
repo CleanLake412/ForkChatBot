553aa712699b40a82ff1beeab761bf8b02f30b11
CleanLake <three_star123@hotmail.com>
2020-11-19 17:50:31 +0900

    コード・リビュー

--- fchatb/js/fcb.js
+++ fchatb/js/fcb.js
@@ -1,23 +1,36 @@
 
-function openFCB(containerID, isPerspective) {
+function openFCB(containerID) {
 
-    var modal = $('#' + containerID),
-        close = $('.md-close'),
+    let modal = $('#' + containerID),
         overlay = $('.md-overlay')[0];
 
-    var jsonData = parseChatForksText(forkText1);
+    // Q&A テキストを解析して分岐データを生成する。
+    let jsonData = parseChatForksText(forkText1);
 
-    function createOptionList(data) {
-        var optionList = '';
-        data.forEach(function (item) {
+    /**
+     * ボットの質問リストからボタンリストを作成する
+     *
+     * @param options 質問リスト
+     * @return {string}
+     */
+    function createOptionList(options) {
+        let optionList = '';
+        options.forEach(function (item) {
             optionList += `<button class="msg-option">` + item.title + `</button>`;
         })
 
         return optionList;
     }
 
-    var childData = undefined;
-    var hasChild = false;
+    let childData = undefined;
+    let hasChild = false;
+
+    /**
+     * ボットの質問を作成する
+     *
+     * @param selected ユーザーが選択した項目
+     * @return {string|string[]}
+     */
     function createBotQuestion(selected = '') {
         if (selected == '') {
             childData = jsonData.children;
@@ -34,64 +47,68 @@ function openFCB(containerID, isPerspective) {
                         </div>
                     </div>`;
         } else {
-            var data;
+            let node;
             childData.forEach(function (item) {
                 if (item.title == selected) {
-                    data = item;
+                    node = item;
                 }
             });
 
-            hasChild = Array.isArray(data.children) && data.children.length > 0;
+            hasChild = Array.isArray(node.children) && node.children.length > 0;
             if (hasChild) {
-                childData = data.children;
+                childData = node.children;
                 hasChild = Array.isArray(childData) && childData.length > 0;
-                return [`<div class="customer-msg new-msg">
-                            <div class="display-flex justify-content-flex-end">
-                                <div class="display-flex">
-                                    <div class="msg-brd">
-                                        <p>` + data.title + `</p>
-                                    </div>
-                                    <div class="lead-triangle-right"></div>
-                                </div>
-                                <!-- <img class="avatar" src="https://placeimg.com/64/64/2"> -->
-                            </div>
-                        </div>`,
-                        `<div class="bot-msg new-msg" style="padding: 10px;">
+                return [
+                    `<div class="customer-msg new-msg">
+                        <div class="display-flex justify-content-flex-end">
                             <div class="display-flex">
-                                <div class="avatar"></div>
-                                <div class="display-flex">
-                                    <div class="lead-triangle-left"></div>
-                                    <div class="msg-brd">
-                                        <p>` + data.instruction.replaceAll('\n', '<br>') + `</p>` + createOptionList(data.children) + `
-                                    </div>
+                                <div class="msg-brd">
+                                    <p>` + node.title + `</p>
+                                </div>
+                                <div class="lead-triangle-right"></div>
+                            </div>
+                            <!-- <img class="avatar" src="https://placeimg.com/64/64/2"> -->
+                        </div>
+                    </div>`,
+                    `<div class="bot-msg new-msg" style="padding: 10px;">
+                        <div class="display-flex">
+                            <div class="avatar"></div>
+                            <div class="display-flex">
+                                <div class="lead-triangle-left"></div>
+                                <div class="msg-brd">
+                                    <p>` + node.instruction.replaceAll('\n', '<br>') + `</p>` + createOptionList(node.children) + `
                                 </div>
                             </div>
-                        </div>`];
+                        </div>
+                    </div>`
+                ];
             } else {
-                return [`<div class="customer-msg new-msg">
-                            <div class="display-flex justify-content-flex-end">
-                                <div class="display-flex">
-                                    <div class="msg-brd">
-                                        <p>` + data.title + `</p>
-                                    </div>
-                                    <div class="lead-triangle-right"></div>
-                                </div>
-                                <!-- <img class="avatar" src="https://placeimg.com/64/64/2"> -->
-                            </div>
-                        </div>`,
-                        `<div class="bot-msg new-msg" style="padding: 10px;">
+                return [
+                    `<div class="customer-msg new-msg">
+                        <div class="display-flex justify-content-flex-end">
                             <div class="display-flex">
-                                <div class="avatar"></div>
-                                <div class="display-flex">
-                                    <div class="lead-triangle-left"></div>
-                                    <div class="msg-brd">
-                                        <p>` + data.instruction.replaceAll('\n', '<br>') + `</p>
-                                        <button class="msg-option md-refresh">もう一度最初から選択する</button>
-                                        <button class="msg-option md-close">閉じる</button>
-                                    </div>
+                                <div class="msg-brd">
+                                    <p>` + node.title + `</p>
+                                </div>
+                                <div class="lead-triangle-right"></div>
+                            </div>
+                            <!-- <img class="avatar" src="https://placeimg.com/64/64/2"> -->
+                        </div>
+                    </div>`,
+                    `<div class="bot-msg new-msg" style="padding: 10px;">
+                        <div class="display-flex">
+                            <div class="avatar"></div>
+                            <div class="display-flex">
+                                <div class="lead-triangle-left"></div>
+                                <div class="msg-brd">
+                                    <p>` + node.instruction.replaceAll('\n', '<br>') + `</p>
+                                    <button class="msg-option md-refresh">もう一度最初から選択する</button>
+                                    <button class="msg-option md-close">閉じる</button>
                                 </div>
                             </div>
-                        </div>`];
+                        </div>
+                    </div>`
+                ];
             }
         }
     }
@@ -102,13 +119,25 @@ function openFCB(containerID, isPerspective) {
         }
     }
 
+    /**
+     * Synchronized sleep
+     *
+     * @param ms
+     * @return {Promise<unknown>}
+     */
     const sleep = ms => {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
+    /**
+     * ユーザーが答えを選択した時の動作
+     *
+     * @param option
+     * @return {Promise<void>}
+     */
     async function selectOption(option) {
         disableAllOptions();
-        var botQuestion = createBotQuestion(option);
+        let botQuestion = createBotQuestion(option);
 
         if (Array.isArray(botQuestion)) {
             await asyncForEach(botQuestion, async function (question) {
@@ -122,7 +151,7 @@ function openFCB(containerID, isPerspective) {
             $('.md-content-box').scrollTop($('.modal-bottom-help-wrap')[0].scrollHeight);
         }
 
-        var options = $('button.msg-option:not(.disabled):not(.md-refresh):not(.md-close)');
+        let options = $('button.msg-option:not(.disabled):not(.md-refresh):not(.md-close)');
         if (options) {
             options.each(function () {
                 $(this).click(function (ev) {
@@ -131,21 +160,24 @@ function openFCB(containerID, isPerspective) {
             });
         }
 
-        refresh = $('.md-refresh:not(.disabled)');
-        if (refresh[0]) {
-            refresh[0].addEventListener('click', async function (ev) {
+        // 「もう一度最初から選択する」ボタンのハンドラー
+        if ($('.md-refresh:not(.disabled)').length > 0) {
+            $('.md-refresh:not(.disabled)')[0].addEventListener('click', async function (ev) {
                 disableAllOptions();
-                var botQuestion = [`<div class="customer-msg new-msg">
-                                        <div class="display-flex justify-content-flex-end">
-                                            <div class="display-flex">
-                                                <div class="msg-brd">
-                                                    <p>もう一度最初から選択する</p>
-                                                </div>
-                                                <div class="lead-triangle-right"></div>
-                                            </div>
-                                            <!-- <img class="avatar" src="https://placeimg.com/64/64/2"> -->
-                                        </div>
-                                    </div>`, createBotQuestion()];
+                let botQuestion = [
+                    `<div class="customer-msg new-msg">
+                        <div class="display-flex justify-content-flex-end">
+                            <div class="display-flex">
+                                <div class="msg-brd">
+                                    <p>もう一度最初から選択する</p>
+                                </div>
+                                <div class="lead-triangle-right"></div>
+                            </div>
+                            <!-- <img class="avatar" src="https://placeimg.com/64/64/2"> -->
+                        </div>
+                    </div>`,
+                    createBotQuestion()
+                ];
 
                 // $('.modal-bottom-help-wrap').html($('.modal-bottom-help-wrap').html().replaceAll('new-msg', '') + createBotQuestion());
                 await asyncForEach(botQuestion, async function (question) {
@@ -154,7 +186,7 @@ function openFCB(containerID, isPerspective) {
                     $('.md-content-box').scrollTop($('.modal-bottom-help-wrap')[0].scrollHeight);
                 });
 
-                var options = $('button.msg-option:not(.disabled)');
+                let options = $('button.msg-option:not(.disabled)');
                 if (options) {
                     options.each(function () {
                         $(this).click(function (ev) {
@@ -165,54 +197,46 @@ function openFCB(containerID, isPerspective) {
             });
         }
 
-        close = $('.md-close:not(.disabled)');
-        if (close[0]) {
-            close[0].addEventListener('click', function (ev) {
+        // 「閉じる」ボタンの動作
+        if ($('.md-close:not(.disabled)').length > 0) {
+            $('.md-close:not(.disabled)')[0].addEventListener('click', function (ev) {
                 ev.stopPropagation();
-                removeModalHandler();
+                removeModal();
             });
         }
     }
 
-    function removeModal(hasPerspective) {
+    function removeModal() {
         modal.removeClass('md-show');
-
-        if (hasPerspective) {
-            $(document.documentElement).removeClass('md-perspective');
-        }
     }
-
-    function removeModalHandler() {
-        removeModal(isPerspective);
-    }
-
-    modal.addClass('md-show');
-    overlay.removeEventListener('click', removeModalHandler);
-    overlay.addEventListener('click', removeModalHandler);
 
     function disableAllOptions() {
         $(".msg-option").addClass("disabled");
         $(".msg-option.disabled").prop('disabled', true);
     }
 
-    if (isPerspective) {
-        setTimeout(function () {
-            $(document.documentElement).addClass('md-perspective');
-        }, 25);
-    }
+    // ダイアログを表示する。
+    modal.addClass('md-show');
+    overlay.removeEventListener('click', removeModal);
+    overlay.addEventListener('click', removeModal);
 
-    $('.modal-bottom-help-wrap').html(createBotQuestion());
-    var options = $('button.msg-option');
-    if (options) {
-        options.each(function () {
-            $(this).click(function (ev) {
-                selectOption(ev.target.innerText);
+    // 500ms 後、ダイアログを表示する
+    $('.modal-bottom-help-wrap').html('');
+    setTimeout(function () {
+        $('.modal-bottom-help-wrap').html(createBotQuestion());
+        let options = $('button.msg-option');
+        if (options) {
+            options.each(function () {
+                $(this).click(function (ev) {
+                    selectOption(ev.target.innerText);
+                });
             });
-        });
-    }
+        }
+    }, 500);
 
+    // ✕ ボタンの処理
     $('.md-content h3 > span')[0].addEventListener('click', function (ev) {
         ev.stopPropagation();
-        removeModalHandler();
+        removeModal();
     });
 }
